193260582fbba75cc161416a2ae5bf818f4f1a14
Dragos Chivulescu <dragosmchivulescu@gmail.com>
2019-02-02 14:18:53 +0000

    Add simple applications that fail with different probabilities

---
+++ TestFiles/AppThatFail/AppThatFailsEveryOtherTime/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace AppThatFailsEveryOtherTime
+{
+    class Program
+    {
+        private static readonly Random random = new Random();
+        private static readonly object syncLock = new object();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simple application that fails every other time(1/2)");
+            int randomNumber;
+            lock (syncLock)
+            {
+                randomNumber = random.Next(0, 2);
+            }
+            if (randomNumber == 1)
+                throw new Exception("The program fails");
+            Thread.Sleep(5000);
+        }
+    }
+}
---
+++ TestFiles/AppThatFail/AppThatFailsEveryThreeOutOfFour/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace AppThatFailsEveryOtherTime
+{
+    class Program
+    {
+        private static readonly Random random = new Random();
+        private static readonly object syncLock = new object();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simple application that fails 75% of the time(3/4)");
+            int randomNumber;
+            lock (syncLock)
+            {
+                randomNumber = random.Next(0, 4);
+            }
+            if (randomNumber == 1)
+                throw new Exception("The program fails");
+            Thread.Sleep(5000);
+        }
+    }
+}
