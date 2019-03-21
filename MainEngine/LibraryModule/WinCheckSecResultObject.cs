22ffdad35bf614b9d60c6922dff1c78e4cea0c78
Dragos Chivulescu <dragosmchivulescu@gmail.com>
2019-03-21 15:05:27 +0000

    Use string builder for report file format

--- MainEngine/LibraryModule/WinCheckSecResultObject.cs
+++ MainEngine/LibraryModule/WinCheckSecResultObject.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.Text;
 
 namespace LibraryModule
 {
+    /// <summary>
+    /// Object encapsulating security compilation properties,
+    /// obtained from WinCheckSec tool.
+    /// </summary>
     public class WinCheckSecResultObject
     {
         public bool DynamicBase { get; set; }
@@ -22,26 +27,34 @@ namespace LibraryModule
         /// <summary>
         /// String format used for the results report
         /// </summary>
-        /// <param name="mode"></param>
+        /// <param name="mode">Basic, Medium or Advanced</param>
         /// <returns>The string representation of the results of WinCheckSec</returns>
         public string ToString(AnalysisMode mode)
         {
-            var stringRepresentation = $"Safe compilation settings: {Environment.NewLine}" 
-                                        + $"DynamicBase: {DynamicBase}{Environment.NewLine}"
-                                        + $"Aslr: {Aslr}{ Environment.NewLine}"
-                                        + $"Nx: {Nx}{ Environment.NewLine}"
-                                        + $"Seh: {Seh}";
+            var stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine("Safe compilation settings:");
+            stringBuilder.AppendLine($"DynamicBase: {DynamicBase}");
+            stringBuilder.AppendLine($"Aslr: {Aslr}");
+            stringBuilder.AppendLine($"Nx: {Nx}");
+            stringBuilder.AppendLine($"Seh: {Seh}");
+
             if (mode != AnalysisMode.Basic)
-                stringRepresentation += $@"{Environment.NewLine}Isolation: {Isolation}{Environment.NewLine}" 
-                        + $"Gs: {Gs}{Environment.NewLine}"
-                        + $"Cfg: {Cfg}";
+            {
+                stringBuilder.AppendLine($"Isolation: {Isolation}");
+                stringBuilder.AppendLine($"Gs: {Gs}");
+                stringBuilder.AppendLine($"Cfg: {Cfg}");
+            }
+
             if (mode == AnalysisMode.Advanced)
-                stringRepresentation += $@"{Environment.NewLine}HighEntropyVa: {HighEntropyVa}{Environment.NewLine}"
-                    + $"ForceIntegrity: {ForceIntegrity}{Environment.NewLine}"
-                    + $"Rfg: {Rfg}{Environment.NewLine}"
-                    + $"SafeSeh: {SafeSeh}{Environment.NewLine}"
-                    + $"Authenticode: {Authenticode}";
-            return stringRepresentation + Environment.NewLine;
+            {
+                stringBuilder.AppendLine($"HighEntropyVa: {HighEntropyVa}");
+                stringBuilder.AppendLine($"ForceIntegrity: {ForceIntegrity}");
+                stringBuilder.AppendLine($"Rfg: {Rfg}");
+                stringBuilder.AppendLine($"SafeSeh: {SafeSeh}");
+                stringBuilder.AppendLine($"Authenticode: {Authenticode}");
+            }
+
+            return stringBuilder.ToString();
         }
     }
 }
